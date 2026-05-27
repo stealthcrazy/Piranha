60df593e49b8ca099279c74b28fae28a98479220
Sohan Prabhu <mail.sohansprabhu@gmail.com>
2026-05-27 17:39:56 +0100

    Added some operators and static methods, needs some fixes to Matmul &

--- include/tensor.hpp
+++ include/tensor.hpp
@@ -6,7 +6,6 @@
 #include <ostream>
 #include <string>
 #include <cassert>
-#include <memory>
 #include <numeric>
 #include <algorithm>
 
@@ -18,7 +17,7 @@ namespace Piranha {
         std::vector<T> obj;
         TensorCore(std::vector<size_t> dims , std::vector<T> v) {
             const size_t size = std::accumulate(dims.begin(), dims.end(), 1, std::multiplies<size_t>());
-            obj =v;
+            obj = std::move(v);
         }
         T* at(size_t i){return &obj[i];}
         T& valueAt(size_t i ){return obj[i];}
@@ -29,14 +28,34 @@ namespace Piranha {
 
     template <typename T> // Tensor class acts as proxy for access
     class Tensor {
-        Tensor(T* start, size_t depth , size_t stride, std::shared_ptr<TensorCore<T>> C,std::vector<size_t> dims) : core{C} , c{start,depth,stride} {
+        Tensor(T* start,
+            size_t stride,
+            std::shared_ptr<TensorCore<T>> C,
+            std::vector<size_t> dims)
+        : core{C} , c{start,stride} {
             shape = dims;
+            shape.erase(shape.begin());
+            size = std::accumulate(dims.begin(),dims.end(),1,std::multiplies<size_t>());
+
         };
+
+        std::vector<T> mult(Tensor a , Tensor b) {
+            std::vector<T> buff;
+            for (int i = 0; i < a.shape[0] ; i++) {
+                for (int j = 0; j < b.shape[1]; j++) {
+                    T p = 0 ;
+                    for  (int k = 0 ; k < b.shape[0]; k++) {
+                        p += *a.core->at( i*a.shape[0] + k) * *b.core->at(k*b.shape[0] + j);
+                    }
+                    buff.push_back(p);
+                }
+            }
+            return buff;
+        }
     public:
         std::shared_ptr<TensorCore<T>> core; // shared ptr for ref counting
         struct Data { // stores accessible data params for Proxy
             T* start;
-            size_t depth;
             size_t stride;
         };
         Data c;
@@ -48,7 +67,7 @@ namespace Piranha {
                 std::cerr << "\n >> Mismatch when declaring Tensor " << std::endl;
                 exit(1);
             }
-            c = {core->at(0),  0 , size};
+            c = {core->at(0), size};
             shape = dims;
         }
         ~Tensor(){} // destructor
@@ -59,44 +78,115 @@ namespace Piranha {
 
 
         Tensor operator[](size_t index) { // indexing operator
-            size_t stride = c.stride/ shape[c.depth];
-            if ( index >= shape[c.depth] ) {
-                std::cerr << "\n>> Index Error " << index << " out of bounds for dim " << stride +1 << "For Piranha_Tensor@" << core.get() << std::endl;
+            size_t stride = c.stride/ shape.at(0);
+            if ( index >= shape.at(0) ) {
+                std::cerr << "\n>> Index Error" << index << " out of bounds for dim " << stride +1 << " For Piranha_Tensor@" << core.get() << std::endl;
                 exit(1);
             }
-            return Tensor((stride * index)+ c.start ,c.depth+1,stride,core); // creates new proxy with updated data
+            return Tensor((stride * index)+ c.start ,stride,core,shape); // creates new proxy with updated data
         }
 
-        template <typename U, size_t M , size_t ... N >
-        Tensor operator+(Tensor<U> r) {
-            if ( ! [](std::is_same<T,U> t){}) {
-                std::cerr << "Type Mismatch";
-            }
-            if (!(std::equal(shape.begin(),shape.end(),r.shape.begin()) & (c.depth == r.c.depth) &  (c.stride == r.c.stride))) {
+        Tensor operator+(Tensor r) {
+            if (!(std::equal(shape.begin(),shape.end(),r.shape.begin())  &  (c.stride == r.c.stride))) {
                 std::cerr << "Mismatch in dimensions for Add";
                 exit(1);
             }
-            std::array<T,c.stride> buff{0};
+            std::vector<T> buff(c.stride);
             for (int i = 0; i< c.stride ; i++ ) {
-                buff[i] = core.at(i) + r.core.at(i);
+                buff.push_back(*(core->at(i)) + *(r.core->at(i)));
+            }
+            return Tensor<T>(shape,buff);
+        }
+        Tensor operator-(Tensor r) {
+            if (!(std::equal(shape.begin(),shape.end(),r.shape.begin())  &  (c.stride == r.c.stride))) {
+                std::cerr << "Mismatch in dimensions for Sub";
+                exit(1);
+            }
+            std::vector<T> buff(c.stride);
+            for (int i = 0; i< c.stride ; i++ ) {
+                buff.push_back(*(core->at(i)) - *(r.core->at(i)));
+            }
+            return Tensor<T>(shape,buff);
+        }
+
+        Tensor operator*(Tensor r) {
+            if (!(std::equal(shape.begin(),shape.end(),r.shape.begin())  &  (c.stride == r.c.stride))) {
+                std::cerr << "Mismatch in dimensions for Element-wise Multiplication";
+                exit(1);
+            }
+            std::vector<T> buff(c.stride);
+            for (int i = 0; i< c.stride ; i++ ) {
+                buff.push_back(*(core->at(i)) * *(r.core->at(i)));
+            }
+            return Tensor<T>(shape,buff);
+        }
+
+        Tensor operator&(Tensor r){ // currently supports 2D & 3D tensors only ( Matmul )
+            if (!(std::equal(shape.begin(),shape.end(),r.shape.begin())  &  (c.stride == r.c.stride))) { // needs fixes as in only hard coded to 2D
+                std::cerr << "Mismatch in dimensions ";
+                exit(1);
             }
 
-            return Tensor<T>();
+            std::vector<size_t> s = shape;
+            s[s.size()-2] = r.shape[s.size()-1];
+            std::vector<T> buff;
+            for (int i  = 0 ; i < 1 ; i++) { // needs fixes as in only hard coded to 2D
+                std::vector<T> temp = mult((*this)[i] ,r[i] );
+                buff.insert(buff.begin(),std::make_move_iterator(temp.begin()),std::make_move_iterator(temp.end()) );
+            }
+
+            return Tensor<T>(s,buff);
         }
-        Tensor operator-(){}
-        Tensor inv(){}
-        Tensor operator*(){}
-        Tensor dot(){}
+
         Tensor scale(){}
         Tensor reshape(){}
         Tensor transpose(){}
 
         // end  of operators
 
-        Tensor arange(){}
-        Tensor ones(){}
-        Tensor diag(){}
+        static Tensor arrange(std::vector<size_t> dims, size_t start , size_t end) {
+            size_t size = std::accumulate(dims.begin(),dims.end(),1,std::multiplies<size_t>());
+            if (size != end-start) {
+                std::cerr << "Hmm !! "; // please change to better debug message
+                exit(1);
+            }
+            std::vector<T> buff;
+            for ( int i = 0 ; i < size ; i++ ) {
+                buff.push_back(i+start);
+            }
+            return Tensor(dims,buff);
+        }
+        static Tensor all(std::vector<size_t> dims, T s) {
+                size_t size = std::accumulate(dims.begin(),dims.end(),1,std::multiplies<size_t>());
+                std::vector<T> buff;
+                for ( int i = 0 ; i < size ; i++ ) {
+                    buff.push_back(s);
+                }
+                return Tensor(dims,buff);
+        }
+        static Tensor ones(std::vector<size_t> dims){ return Tensor::all(std::move(dims),1);}
+        static Tensor zeros(std::vector<size_t> dims){ return Tensor::all(std::move(dims),0);}
 
+        static Tensor diag(std::vector<size_t> dims, T s) {
+            size_t size = std::accumulate(dims.begin(),dims.end(),1,std::multiplies<size_t>());
+            std::vector<T> buff;
+            size_t ndm = dims.size();
+            size_t m = dims[ndm-2];
+            size_t n = dims[ndm-1];
+            for ( int i = 0 ; i < size / (m*n); i++ ) {
+                for (int j = 0; j < m; j++) {
+                    for (int k = 0; k < n; k++) {
+                        if (j == k) {
+                            buff.push_back(s);
+                        }else {
+                            buff.push_back(0);
+                        }
+                    }
+                }
+            }
+            return Tensor(dims,buff);
+
+        }
 
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,10 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
-    Piranha::Tensor<float> t({3,3},{1,2,3,4,5,6,7,8,9});
-    Piranha::Tensor<float> t1({3,3},{1,2,3,4,5,6,7,8,9});
-    std::cout << t[0][0].value();
+    Piranha::Tensor<float> t1 = Piranha::Tensor<float>::ones({3,3});
+    Piranha::Tensor<float> t2({3,3},{1,2,3,4,5,6,7,8,9});
 
+    std::cout << (t1 & t2)[1][2].value();
 
     return 0;
 }
